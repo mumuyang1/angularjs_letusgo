cfe9a51e1668c16f49d5d4ed3a79f00c185efead
mumuyang1 <843624084@qq.com>
2014-08-27 09:50:51 +0800

    all button

--- app/scripts/controllers/cart.js
+++ app/scripts/controllers/cart.js
@@ -11,36 +11,47 @@ angular.module('anLetusgoApp')
         _.forEach(cartProduct,function(cartProduct){
           if(cartProduct.items.name === cartItem.name){
           cartProduct.inputCount += 1;
-          cartSums =  $scope.cartsums++;
+          $scope.cartsums++;
           $scope.$parent.addCartSum(cartItem);
           }
         });
         $scope.total = getTotal(cartProduct);
-        console.log($scope.total);
         localStorage.setItem('cartProduct',JSON.stringify(cartProduct));
         localStorage.setItem('cartSum',JSON.stringify($scope.cartsums));
-
       };
-      $scope.reduceButton = function(cartItem){};
+
       $scope.reduceButton = function(cartItem){
         _.forEach(cartProduct,function(cartProduct){
           if(cartProduct.items.name === cartItem.name){
             if(cartProduct.inputCount <= 1){
-              cartProduct.inputCount = 1;
+               cartProduct.inputCount = 1;
             }else{
-            cartProduct.inputCount -= 1;
-          }
-            cartSums =  $scope.cartsums--;
+              cartProduct.inputCount -= 1;
+              $scope.cartsums--;
+              console.log(cartSums);
+             $scope.$parent.addCartSum(cartItem);
+            }
           }
         });
         $scope.total = getTotal(cartProduct);
         localStorage.setItem('cartProduct',JSON.stringify(cartProduct));
         localStorage.setItem('cartSum',JSON.stringify($scope.cartsums));
       };
-      // $scope.deleteButton = function(){};
+
+      $scope.deleteButton = function(cartItem){
+        for(var i = 0; i < cartProduct.length; i++){
+          if(cartProduct[i].items.name === cartItem.name){
+           $scope.cartsums = $scope.cartsums - cartProduct[i].inputCount;
+           $scope.$parent.addCartSum(cartItem);
+           cartProduct = _.without(cartProduct,cartProduct[i]);
+          }
+        }
+        $scope.cartItems = cartProduct;
+        $scope.total = getTotal(cartProduct);
+        localStorage.setItem('cartProduct',JSON.stringify(cartProduct));
+        localStorage.setItem('cartSum',JSON.stringify($scope.cartsums));
+      }
     });
-
-
 
 function getTotal(cartProduct){
     var total = 0;
@@ -50,25 +61,3 @@ function getTotal(cartProduct){
 
     return total;
 }
-
-
-
-// function deleteButton(id){
-//
-//     var cartItems = JSON.parse(localStorage.getItem('cartProduct'));
-//     var cartSums = JSON.parse(localStorage.getItem('cartSum'));
-//     if(!cartItems){
-//         cartItems = [];
-//     }
-//     for(var i = 0; i < cartItems.length; i++) {
-//         if (id === cartItems[i].items.barcode) {
-//             cartSums = cartSums - cartItems[i].inputCount ;
-//             cartItems[i].inputCount = 0;
-//
-//             $('#'+ id).val( cartItems[i].inputCount);
-//             $('#cartItemSum').text(cartSums);
-//         }
-//     }
-//     localStorage.setItem('cartProduct',JSON.stringify(cartItems));
-//     localStorage.setItem('cartSum',JSON.stringify(cartSums));
-//   }
