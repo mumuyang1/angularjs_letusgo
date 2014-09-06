5fee9ef0cd2fb2418f2da0f6f91b923e73d4a3d9
mumuyang1 <843624084@qq.com>
2014-09-06 18:06:27 +0800

    a little changing

--- app/scripts/controllers/categoryManage.js
+++ app/scripts/controllers/categoryManage.js
@@ -1,8 +1,12 @@
 'use strict';
 
 angular.module('anLetusgoApp')
+
   .controller('CategoryManageCtrl', function ($scope) {
 
     $scope.$emit('to-parent-productManageActive');
+      $scope.category = {
+      id : 1
+  };
 
   });
